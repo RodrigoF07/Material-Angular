b8c379275058fc96f03c843d318c950e4ad8fd20
Técnico em Informática 2021.2 <36127512021.2@senacrjedu.local>
2022-11-17 12:02:24 -0300

    version_001

---
+++ Aula08/objeto.js
@@ -0,0 +1,17 @@
+var cliente = {
+    nome:"Rodrigo",
+    cpf:"000000",
+    endereco:"Rua 30"
+}
+
+var clientes = [
+    {
+        nome:"Rodrigo",
+        cpf:"000000",
+        endereco:"Rua 30"
+    },
+    {
+
+    }
+
+]
